f6445878029e6a6a5f388d5b9aa0c0d8dc223db9
Plouik <olivier.desaintmartin@reseau.eseo.fr>
2019-06-18 18:50:08 +0000

    test

--- somme.js
+++
@@ -1,9 +0,0 @@
-const sum = (num1,num2) => num1 + num2;
-const PI = 3.14;
-class Something{
-    constructor(){
-        console.log('constructor');
-    }
-}
-module.exports = {sum : sum,PI : PI,Something : Something};
-
